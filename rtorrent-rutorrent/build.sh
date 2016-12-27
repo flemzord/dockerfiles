20e5f1fb6129aa76ba7d8fecdd329fa04b5e7ed4
Maxence Maireaux <maxence@realstorm.fr>
2016-12-27 19:48:38 +0100

    Remove DOCKER_EMAIL & Refonte rtorrent-rutorrent

--- rtorrent-rutorrent/build.sh
+++ rtorrent-rutorrent/build.sh
@@ -6,12 +6,8 @@ DOCKER_PUSH=$1
 
 if [ "$DOCKER_PUSH" == "build" ]; then
 	docker build -t flemzord/rtorrent-rutorrent:latest $FOLDER
-	docker build --build-arg WITH_FILEBOT=YES -t flemzord/rtorrent-rutorrent:filebot $FOLDER
-	docker tag flemzord/rtorrent-rutorrent:filebot flemzord/rtorrent-rutorrent:latest-filebot
 fi
 
 if [ "$DOCKER_PUSH" == "push" ]; then
-  for tag in latest filebot latest-filebot; do
-    docker push flemzord/rtorrent-rutorrent:$tag
-  done
+    docker push flemzord/rtorrent-rutorrent:latest
 fi
