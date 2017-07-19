e0db11955cd5abfe782527e52bdb3c526adaf8c5
Maxence Maireaux <maxence@realstorm.fr>
2017-07-19 18:36:54 +0200

    Update build.sh

--- caddy/build.sh
+++ caddy/build.sh
@@ -4,9 +4,9 @@ FOLDER=$(dirname $0)
 DOCKER_PUSH=$1
 
 if [ "$DOCKER_PUSH" == "build" ]; then
-   docker build -t flemzord/caddy:latest $FOLDER
+   #docker build -t flemzord/caddy:latest $FOLDER
 fi
 
 if [ "$DOCKER_PUSH" == "push" ]; then
-   docker push flemzord/caddy:latest
+   #docker push flemzord/caddy:latest
 fi
